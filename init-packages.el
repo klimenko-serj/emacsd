09766605f966055e7424ff9eb7c70371a72f204c
Serj Klimenko <klimenko.serj@gmail.com>
2017-12-13 15:03:10 +0200

    initial commit

---
+++ init-packages.el
@@ -0,0 +1,30 @@
+(require 'package)
+
+(add-to-list 'package-archives
+             '("elpy" . "http://jorgenschaefer.github.io/packages/"))
+
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+
+(add-to-list 'package-archives
+             '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t)
+
+(add-to-list 'load-path "~/.emacs.d/site-lisp/")
+
+
+(setq package-list
+      '(gruvbox-theme
+	evil))
+
+
+; activate all the packages
+(package-initialize)
+
+; fetch the list of packages available
+(unless package-archive-contents
+  (package-refresh-contents))
+
+; install the missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
